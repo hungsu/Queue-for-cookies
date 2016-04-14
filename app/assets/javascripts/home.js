dc08fa56cb25b971e263a62dd58d595f9f3c7d99
Hung-Su Nguyen <hnguyen@politico.com>
2016-04-14 03:59:17 -0400

    Use Array.filter instead of Array.splice to remove element

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,10 +1,3 @@
-// Enable pusher logging - don't include this in production
-Pusher.log = function(message) {
-  if (window.console && window.console.log) {
-    window.console.log(message);
-  }
-};
-
 // formAuthenticityToken is a global set in application.html.erb
 var pusher = new Pusher('16875e25b84dc444396e', {
   authEndpoint: '/pusher/auth',
@@ -27,14 +20,11 @@ else
 */
 var channel = pusher.subscribe('presence-biscuits');
 
-// TODO Quota should not be in clientside code. Find another way to determine if person is eligible for cookie or not
 var quota = 1;
 var peopleAheadOfMe = [];
 
 channel.bind('pusher:subscription_succeeded', function(members) {
-
   // if members count at time of subscription is less than QUOTA, give a cookie
-  console.log(members.count,quota);
   if (members.count <= quota) {
     giveCookie();
   } else {
@@ -50,10 +40,9 @@ channel.bind('pusher:subscription_succeeded', function(members) {
 
 channel.bind('pusher:member_removed', function(member) {
   // if member removed was a person ahead of me in line, update my position in line
-  var index = peopleAheadOfMe.indexOf(member.id);
-  if (index >= 0) {
-    peopleAheadOfMe.splice(index,1);
-  }
+  peopleAheadOfMe = peopleAheadOfMe.filter(function(value){
+    return value != member.id;
+  });
   console.log('People ahead of me:', peopleAheadOfMe);
 
   // If I am now in position for a cookie, give me a cookie
@@ -64,6 +53,6 @@ channel.bind('pusher:member_removed', function(member) {
 
 function giveCookie() {
   alert('Have a cookie ' + channel.members.me.info.name + '!');
-  // Unsubscribe from events - e.g. don't give me a cookie again if another member joins and leaves
+  // Unsubscribe from events - e.g. don't give me a cookie again if another member leaves
   channel.unbind('pusher:member_removed');
 }
