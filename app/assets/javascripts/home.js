308e595cd9eeb76f1b373de5d36614c1d3afae44
Hung-Su Nguyen <hnguyen@politico.com>
2016-04-14 11:28:38 -0400

    Initialise widget only if on home page

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,58 +1,78 @@
-// formAuthenticityToken is a global set in application.html.erb
-var pusher = new Pusher('16875e25b84dc444396e', {
-  authEndpoint: '/pusher/auth',
-  auth: {
-    headers: {
-      'X-CSRF-Token': formAuthenticityToken
-    }
-  }
+$(document).ready(function(){
+	var $widget = $('.js-cookie-widget');
+
+	// Only initialise widget if widget element is on page
+	if ($widget.length > 0) {
+		// formAuthenticityToken is a global set in application.html.erb
+		var pusher = new Pusher('16875e25b84dc444396e', {
+			authEndpoint: '/pusher/auth',
+			auth: {
+				headers: {
+					'X-CSRF-Token': formAuthenticityToken
+				}
+			}
+		});
+
+		var channel = pusher.subscribe('presence-cookies');
+
+		var quota = 1;
+		var peopleAheadOfMe = [];
+
+		/*
+		When user logs into app
+		If number of current users is less than the app's quota, give this user a cookie
+		Otherwise:
+		  build an array of users in line ahead of this user, (all current users except this user)
+		  Let this user know they are in a queue.
+		*/
+		channel.bind('pusher:subscription_succeeded', function(members) {
+			if (members.count <= quota) {
+			  giveCookie();
+			} else {
+			  members.each(function(member) {
+				if (member.id != members.myID) {
+				  peopleAheadOfMe.push(member.id);
+				}
+			  });
+
+			  $widget[0].textContent = positionInLine(peopleAheadOfMe.length - quota);
+			}
+		});
+
+		channel.bind('pusher:member_removed', function(member) {
+			// if member removed was a person ahead of me in line, update my position in line
+			peopleAheadOfMe = peopleAheadOfMe.filter(function(value){
+				return value != member.id;
+			});
+
+			// If I am now in position for a cookie, give me a cookie
+			if (peopleAheadOfMe.length < quota) {
+				giveCookie();
+			} else {
+				$widget[0].textContent = positionInLine(peopleAheadOfMe.length - quota);
+			}
+		});
+
+		function giveCookie() {
+			$widget[0].textContent = 'Have a cookie ' + channel.members.me.info.name + '!';
+			// Unsubscribe from events - e.g. don't give me a cookie again if another member leaves
+			channel.unbind('pusher:member_removed');
+		}
+
+		function positionInLine(position) {
+			var suffixes = ['st','nd','rd','th','th','th','th','th','th','th'];
+			var positionString = "";
+			if (position == 0) {
+			  positionString = "next";
+			} else {
+			  positionString = (position+1) + suffixes[position % 10];
+			}
+			return "You're " + positionString + " in line for a cookie.";
+		}
+
+		var debugMessages = pusher.subscribe("test_channel");
+			debugMessages.bind('my_event', function(data) {
+			console.log(data.message);
+		});
+	}
 });
-
-
-/*
-Arrive on page
-join queue (channel)
-If position in queue is less than quota
-  "have a cookie"
-else
-  wait for even "member removed"
-  check again
-*/
-var channel = pusher.subscribe('presence-biscuits');
-
-var quota = 1;
-var peopleAheadOfMe = [];
-
-channel.bind('pusher:subscription_succeeded', function(members) {
-  // if members count at time of subscription is less than QUOTA, give a cookie
-  if (members.count <= quota) {
-    giveCookie();
-  } else {
-    // add each member in members to people ahead of me, excluding myself
-    members.each(function(member) {
-      if (member.id != members.myID) {
-        peopleAheadOfMe.push(member.id);
-      }
-    });
-  }
-  console.log('People ahead of me:', peopleAheadOfMe);
-});
-
-channel.bind('pusher:member_removed', function(member) {
-  // if member removed was a person ahead of me in line, update my position in line
-  peopleAheadOfMe = peopleAheadOfMe.filter(function(value){
-    return value != member.id;
-  });
-  console.log('People ahead of me:', peopleAheadOfMe);
-
-  // If I am now in position for a cookie, give me a cookie
-  if (peopleAheadOfMe.length < quota) {
-    giveCookie();
-  }
-});
-
-function giveCookie() {
-  alert('Have a cookie ' + channel.members.me.info.name + '!');
-  // Unsubscribe from events - e.g. don't give me a cookie again if another member leaves
-  channel.unbind('pusher:member_removed');
-}
